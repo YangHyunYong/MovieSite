8d34b72bed53e1109d214ed23531feed68a92576
YangHyunYong <9938ok@naver.com>
2021-01-11 19:36:56 +0900

    ~Movie Detail

---
+++ client/src/components/views/commons/GridCards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Col} from 'antd'
+
+
+function GridCards(props) {
+    return (
+        <div>
+            <Col lg={6} md={8} xs={12}>
+                <div style={{position: 'relative'}}>
+                    <a href={`/movie/${props.movieId}`}>
+                        <img style={{width:'100%', height: '320px'}} src={props.image} alt={props.movieName}></img>
+                    </a>
+                </div>
+            </Col>            
+        </div>
+    )
+}
+
+export default GridCards
